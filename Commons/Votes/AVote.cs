009acb719877d5152cdd20966f4361f324a6d8f3
Achim Friedland <github@ahzf.de>
2012-02-08 23:07:48 +0100

    Some clean'up.

--- Commons/Votes/AVote.cs
+++ Commons/Votes/AVote.cs
@@ -2,11 +2,11 @@
  * Copyright (c) 2010-2012 Achim 'ahzf' Friedland <code@ahzf.de>
  * This file is part of Illias Commons <http://www.github.com/ahzf/Illias>
  *
- * Licensed under the General Public License, Version 3.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.gnu.org/licenses/gpl.html
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
