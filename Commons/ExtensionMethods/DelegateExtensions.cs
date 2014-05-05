085b5a37800d767ffaf40368bcb1d0e3364a10bf
Achim Friedland <github@ahzf.de>
2014-05-05 23:03:10 +0200

    updates

--- Commons/ExtensionMethods/DelegateExtensions.cs
+++ Commons/ExtensionMethods/DelegateExtensions.cs
@@ -68,6 +68,27 @@ namespace eu.Vanaheimr.Illias.Commons
 
         #endregion
 
+        #region FailSafeInvoke<T>(this Action, Argument1, Argument2)
+
+        /// <summary>
+        /// Run the given delegate without worrying about multi-threading side-effects.
+        /// </summary>
+        /// <typeparam name="T">The type of the delegate to run.</typeparam>
+        /// <param name="Action">A delegate of type T.</param>
+        /// <param name="Argument1">The first parameter of the delegate.</param>
+        /// <param name="Argument2">The second parameter of the delegate.</param>
+        public static void FailSafeInvoke<T1, T2>(this Action<T1, T2> Action, T1 Argument1, T2 Argument2)
+        {
+
+            var ActionCopy = Action;
+
+            if (ActionCopy != null)
+                ActionCopy(Argument1, Argument2);
+
+        }
+
+        #endregion
+
     }
 
 }
