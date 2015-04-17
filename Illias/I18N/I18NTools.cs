b1c8ea3438d5d203bcbd2c777ee032a46fdcbf94
Achim Friedland <github@ahzf.de>
2015-04-17 06:06:37 +0200

    Added Timestamped<...>

--- Illias/I18N/I18NTools.cs
+++ Illias/I18N/I18NTools.cs
@@ -1,134 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * Copyright (c) 2010-2015 Achim 'ahzf' Friedland <achim@graphdefined.org>
+ * This file is part of Illias <http://www.github.com/Vanaheimr/Illias>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
 using System.Linq;
-using System.Text;
+
+#endregion
 
 namespace org.GraphDefined.Vanaheimr.Illias
 {
 
-    public struct German
-    {
-
-        public readonly String Text;
-
-        public German(String myText)
-        {
-            Text = myText;
-        }
-
-        public override String ToString()
-        {
-            return Text;
-        }
-
-    }
-
-    public struct English
-    {
-
-        public readonly String Text;
-
-        public English(String myText)
-        {
-            Text = myText;
-        }
-
-        public override String ToString()
-        {
-            return Text;
-        }
-
-    }
-
-
-    public interface JSONString
-    {
-        String JSONString { get; }
-    }
-
-    public struct I18N : JSONString
-    {
-
-        public readonly English English;
-        public readonly German  German;
-
-        public I18N(English myEnglish)
-        {
-            English = myEnglish;
-            German  = new German(myEnglish.Text);
-        }
-
-        public I18N(English myEnglish, German myGerman)
-        {
-            English = myEnglish;
-            German  = myGerman;
-        }
-
-        public override String ToString()
-        {
-            return String.Concat("en: ", English, ", de:", German);
-        }
-
-
-        public String JSONString
-        {
-            get
-            {
-                return String.Concat(@"{ ""en"": """, English, @""", ""de"": """, German, @""" }");
-            }
-        }
-
-    }
-
-
+    /// <summary>
+    /// Tools for internationalized (I18N) text/string.
+    /// </summary>
     public static class I18NTools
     {
 
-        public static String ToHTML(this I18N I18NValue)
+        #region ToHTML(this I18NString)
+
+        /// <summary>
+        /// Convert the given internationalized (I18N) text/string to HTML.
+        /// </summary>
+        /// <param name="I18NString">An internationalized (I18N) text/string.</param>
+        public static String ToHTML(this I18NString I18NString)
         {
 
-            return "<span class=\"english\">" + I18NValue.English.Text + "</span>" +
-                   "<span class=\"german\">"  + I18NValue.German. Text + "</span>";
+            return I18NString.
+                       Select(v => @"<span class=""I18N_" + v.Language + @""">" + v.Text + "</span>").
+                       AggregateWith(Environment.NewLine);
 
-        }
-
-        public static String ToLink(this I18N I18NValue, String Link)
-        {
-
-            return "<span class=\"english\"><a href = \"" + Link + "?language=en\">" + I18NValue.English.Text + "</a></span>" +
-                   "<span class=\"german\" ><a href = \"" + Link + "?language=de\">" + I18NValue.German.Text  + "</a></span>";
-
-        }
-
-        public static String ToHTML(this I18N I18NValue, String Prefix, String Postfix)
-        {
-
-            return "<span class=\"english\">" + Prefix + I18NValue.English.Text + Postfix + "</span>" +
-                   "<span class=\"german\">"  + Prefix + I18NValue.German. Text + Postfix + "</span>";
-
-        }
-
-        #region AppendI18N(this StringBuilder, I18NValue)
-
-        public static StringBuilder AppendI18N(this StringBuilder StringBuilder,
-                                               I18N I18NValue)
-        {
-            return StringBuilder.Append(I18NValue.ToHTML());
         }
 
         #endregion
 
-        #region AppendI18N(this StringBuilder, Prefix, I18NValue, Postfix)
+        #region ToHTML(this I18NString, Prefix, Postfix)
 
-        public static StringBuilder AppendI18N(this StringBuilder StringBuilder,
-                                               String Prefix,
-                                               I18N   I18NValue,
-                                               String Postfix)
+        /// <summary>
+        /// Convert the given internationalized (I18N) text/string to HTML.
+        /// </summary>
+        /// <param name="I18NString">An internationalized (I18N) text/string.</param>
+        /// <param name="Prefix">A prefix.</param>
+        /// <param name="Postfix">A postfix.</param>
+        public static String ToHTML(this I18NString I18NString, String Prefix, String Postfix)
         {
 
-            return StringBuilder.Append(Prefix).
-                                 Append(I18NValue.ToHTML()).
-                                 Append(Postfix);
+            return I18NString.
+                       Select(v => @"<span class=""I18N_" + v.Language + @""">" + Prefix + v.Text + Postfix + "</span>").
+                       AggregateWith(Environment.NewLine);
+
+        }
+
+        #endregion
+
+        #region ToHTMLLink(this I18NString, String URI)
+
+        /// <summary>
+        /// Convert the given internationalized (I18N) text/string to a HTML link.
+        /// </summary>
+        /// <param name="I18NString">An internationalized (I18N) text/string.</param>
+        /// <param name="URI">An URI.</param>
+        public static String ToHTMLLink(this I18NString I18NString, String URI)
+        {
+
+            return I18NString.
+                       Select(v => @"<span class=""I18N_" + v.Language + @"""><a href=""" + URI + @"?language=en"">" + v.Text + "</a></span>").
+                       AggregateWith(Environment.NewLine);
+
+        }
+
+        #endregion
+
+        #region ToJSON(this I18NString)
+
+        /// <summary>
+        /// Convert the given internationalized (I18N) text/string to JSON.
+        /// </summary>
+        /// <param name="I18NString">An internationalized (I18N) text/string.</param>
+        public static String ToJSON(this I18NString I18NString)
+        {
+
+            return "{" + Environment.NewLine +
+                   I18NString.
+                       Select(v => @"  """ + v.Language + @""": """ + v.Text + @""",").
+                       AggregateWith(Environment.NewLine) +
+                   "}";
 
         }
 
---
+++ Illias/Time/Timestamped.cs
@@ -0,0 +1,197 @@
+﻿/*
+ * Copyright (c) 2010-2015 Achim 'ahzf' Friedland <achim@graphdefined.org>
+ * This file is part of Illias <http://www.github.com/Vanaheimr/Illias>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace org.GraphDefined.Vanaheimr.Illias
+{
+
+    #region Timestamped<T>
+
+    /// <summary>
+    /// A value with its creation timestamp.
+    /// </summary>
+    /// <typeparam name="T">The type of the timestamped value.</typeparam>
+    public struct Timestamped<T>
+    {
+
+        #region Properties
+
+        #region Timestamp
+
+        private readonly DateTime _Timestamp;
+
+        /// <summary>
+        /// The timestamp of the value creation.
+        /// </summary>
+        public DateTime Timestamp
+        {
+            get
+            {
+                return _Timestamp;
+            }
+        }
+
+        #endregion
+
+        #region Value
+
+        private readonly T _Value;
+
+        /// <summary>
+        /// The value.
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                return _Value;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region Timestamped(Value)
+
+        /// <summary>
+        /// Create a new timestamped value.
+        /// </summary>
+        /// <param name="Value">The value.</param>
+        public Timestamped(T Value)
+            : this(Value, DateTime.Now)
+        { }
+
+        #endregion
+
+        #region Timestamped(Value, Timestamp)
+
+        /// <summary>
+        /// Create a new timestamped value.
+        /// </summary>
+        /// <param name="Value">The value.</param>
+        /// <param name="Timestamp">The timestamp.</param>
+        public Timestamped(T Value, DateTime Timestamp)
+        {
+            _Value      = Value;
+            _Timestamp  = Timestamp;
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+
+    #endregion
+
+    #region Timestamped_RW<T>
+
+    /// <summary>
+    /// A value with its creation timestamp.
+    /// </summary>
+    /// <typeparam name="T">The type of the timestamped value.</typeparam>
+    public struct Timestamped_RW<T>
+    {
+
+        #region Properties
+
+        #region Timestamp
+
+        private DateTime _Timestamp;
+
+        /// <summary>
+        /// The timestamp of the value creation.
+        /// </summary>
+        public DateTime Timestamp
+        {
+            get
+            {
+                return _Timestamp;
+            }
+        }
+
+        #endregion
+
+        #region Value
+
+        private readonly T _Value;
+
+        /// <summary>
+        /// The value.
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                return _Value;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region Timestamped_RW(Value)
+
+        /// <summary>
+        /// Create a new timestamped value.
+        /// </summary>
+        /// <param name="Value">The value.</param>
+        public Timestamped_RW(T Value)
+            : this(Value, DateTime.Now)
+        { }
+
+        #endregion
+
+        #region Timestamped(Value, Timestamp)
+
+        /// <summary>
+        /// Create a new timestamped value.
+        /// </summary>
+        /// <param name="Value">The value.</param>
+        /// <param name="Timestamp">The timestamp.</param>
+        public Timestamped_RW(T Value, DateTime Timestamp)
+        {
+            _Value      = Value;
+            _Timestamp  = Timestamp;
+        }
+
+        #endregion
+
+        #endregion
+
+
+        public void UpdateTimestamp()
+        {
+            _Timestamp = DateTime.Now;
+        }
+
+    }
+
+    #endregion
+
+}
