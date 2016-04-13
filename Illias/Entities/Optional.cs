e849c77f4249af97d335228654220e30b66c9ac9
Achim Friedland <github@ahzf.de>
2016-04-13 20:19:38 +0200

    Updates

--- Illias/Entities/Optional.cs
+++ Illias/Entities/Optional.cs
@@ -24,6 +24,7 @@ using System;
 namespace org.GraphDefined.Vanaheimr.Illias
 {
 
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public class OptionalAttribute : Attribute
     {
 
